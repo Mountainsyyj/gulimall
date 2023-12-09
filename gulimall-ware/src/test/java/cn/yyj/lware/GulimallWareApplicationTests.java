7a9c9f4d8595977ef157b785a6242bbf5adca9fb
yyj <609082367@qq.com>
2023-12-09 11:22:25 +0800

    #init guli mall project.

---
+++ gulimall-ware/src/test/java/cn/yyj/lware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yyj.lware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
